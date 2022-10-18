2c728910cba6a8d5447d7e212383cf547a9628be
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-10-18 08:59:24 +0300

    3 exerceises added to while loop folder

---
+++ WhileLoop/While_Exercise_3.py
@@ -0,0 +1,37 @@
+"""
+
+start = 0
+end = 3
+terminate condition ->
+counter
+
+"""
+
+"""
+numOfTestCases = int(input())  # 3
+
+start = 0
+end = numOfTestCases - 1
+
+while start <= end:
+
+    # Block of Code - Start
+    num1 = int(input())
+    num2 = int(input())
+    print(num1 + num2)
+    # Block of Code - End
+
+    start += 1
+
+# Assignment
+
+"""
+
+numOfTestCases = int(input())
+start = numOfTestCases
+end = 0
+while start > end:
+    num1 = int(input())
+    num2 = int(input())
+    print(num1+num2)
+    start -= 1
