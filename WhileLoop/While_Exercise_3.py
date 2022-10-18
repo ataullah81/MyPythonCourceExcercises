6eaaf957af1c930cb34d93b3ed0098428bb2ab06
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-10-18 10:54:57 +0300

    While loop exercise modified

--- WhileLoop/While_Exercise_3.py
+++ WhileLoop/While_Exercise_3.py
@@ -26,6 +26,7 @@ while start <= end:
 # Assignment
 
 """
+"""
 
 numOfTestCases = int(input())
 start = numOfTestCases
@@ -35,3 +36,18 @@ while start > end:
     num2 = int(input())
     print(num1+num2)
     start -= 1
+    
+"""
+num_test_case = int(input("How many time you would like to loop: "))
+print("You would like to loop (", num_test_case, ") times.")
+start = 0
+while start < num_test_case:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    print(num1+num2)
+    start +=1
+
+    if num_test_case > start:
+        print("You have (",num_test_case - start,") times left.")
+    else:
+        print("You finished your looping, bye bye!!")
