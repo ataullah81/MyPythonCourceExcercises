9f28bb3dca9e44183756dc5ca8ff43cc8a5b8e59
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-11-03 17:57:31 +0200

    exam5 modified

--- Exam/exam-05_continue_looping.py
+++ Exam/exam-05_continue_looping.py
@@ -1,17 +1,18 @@
 # Provided a list where every element occurs doubled except for one. Find that single one.
+
 isContinue = True
-start = 0
 while isContinue:
-    given_lst = list(map(int, input("Enter numbers with space: \n").split()))
-    single_value = []
-    for i in given_lst:
-        # checking elements in list, which is not exist more than one time
-        if given_lst.count(i) < 2:
-            single_value.append(i)
-    print("Unique value:", single_value, sep='\n')
-    inp = input("Write 'ok' to continue and 'no' to terminate: \n")
-    if inp == 'ok':
-        continue
-    elif inp == 'no':
-        break
-start += 1
+    try:
+        inp = input("Write 'ok' to continue and 'no' to terminate: \n")
+        if inp == 'ok':
+            given_lst = list(map(int, input("Enter numbers with space: \n").split()))
+            single_value = []
+            for i in given_lst:
+                # checking elements in list, which is not exist more than one time
+                if given_lst.count(i) < 2:
+                    single_value.append(i)
+            print("Unique value:", single_value, sep='\n')
+        elif inp == 'no':
+            break
+    except Exception as ex:
+        print("Enter valid numbers.")
